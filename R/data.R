b2a71d60d361aebc7a7d8bbca0e6af56bf0f770e
Andrew Hinton <68709832+andrew84830813@users.noreply.github.com>
2021-09-13 05:57:02 -0400

    Added tutorial dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' 16S sequenced cerebrospinal fluid (CSF) case-control dataset
+#'
+#' A microbiome dataset containing the case-control status and Operational Taxonomic Unit (OTU) count composition of infants with/without Postinfectious hydrocephalus (PIH).
+#' Dataset was extracted from the microbiomeDB release 22 and pre-processed in:
+#'  \url{https://github.com/andrew84830813/SelectionEnergyPerm_Project.git}.
+#'
+#'
+#'
+#' @format A data frame with 92 rows (samples) and 369 variables( Status[pos (PIH) / neg (NPIH)] + OTUs[full taxonomic assignment] ):
+
+#' @source \url{https://microbiomedb.org/mbio/app/record/dataset/DS_953b8ff2d4}
+"Uguanda_PIH"
